7d35d7334a35ea2701e1048839954f9696204803
janbolot-web <janbolotcode@gmail.com>
2022-06-05 19:09:32 +0600

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const config = require("config");
 const { mongoose } = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-const PORT = config.get("port");
+const PORT = process.env.PORT ||config.get("port");
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth.routes"));
